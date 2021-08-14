157c5d5e6f50f9d71f732bf91a2daa57f1d76944
Alejandro <acuellarh@gmail.com>
2021-08-14 16:59:39 -0500

    Clase 4 - Componentes

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import React from "react"
+import logo from './logo.svg';
+import './App.css';
+import Componente from './components/Componente'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <section>
+        <Componente msg="Hola soy un componente funcional expresado desde una prop"/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
